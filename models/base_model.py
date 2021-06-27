e39443fe4a8ef3b397029194849c4190bc3a35b9
Brian Zapata <dzapata397@gmail.com>
2021-06-27 01:32:54 +0000

    Created condition to the kwargs and the test to the point 4 Commit

--- models/base_model.py
+++ models/base_model.py
@@ -10,16 +10,26 @@ class BaseModel:
         This class defines all common attributes/methods for other classes
     """
 
-    def __init__(self, id=None, created_at=None, updated_at=None):
+    def __init__(self, *args, **kwargs):
         """Initial method that store the attributes
         args:
             id: The UUID to identify the instances
             created_at: to assign the datetime to an instance
             updated_at: to update the datetime of an instance
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key is '__class__':
+                    continue
+                if key in ('created_at', 'updated_at'):
+                    date = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, date)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Human reading
@@ -30,7 +40,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at with the current datetime
         """
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance
