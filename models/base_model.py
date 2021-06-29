3a08cc850d059f83e60a661536ab08ad902b9b34
Andrecast <acastrillonpuerta@gmail.com>
2021-06-29 17:29:50 -0500

    More comments

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,8 @@ class BaseModel:
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime
+        """updates the public instance attribute
+        updated_at with the current datetime
         """
         self.updated_at = datetime.now()
         models.storage.save()
