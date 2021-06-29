679484802f4738600b31f35680a7ffdaf072d75b
Brian Zapata <dzapata397@gmail.com>
2021-06-29 00:46:18 +0000

    Organized the condition of kwargs Commit

--- models/base_model.py
+++ models/base_model.py
@@ -22,12 +22,9 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
-            if 'id' in kwargs.keys():
-                self.id = kwargs['id']
-            if 'created_at' in kwargs.keys():
-                self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            if 'updated_at' in kwargs.keys():
-                self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                if key in ('created_at', 'updated_at'):
+                    date = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, date)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
