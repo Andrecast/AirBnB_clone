08ff22cd0898876da5dd49259a9c52c3f9f7bacd
Brian Zapata <dzapata397@gmail.com>
2021-07-01 04:14:12 +0000

    Changes Commit-2

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,93 +1,55 @@
 #!/usr/bin/python3
-""" Import unittest and created a class for unit test """
+"""
+    Test for base FileStorage
+"""
+import unittest
+from models.engine import file_storage
+from models import storage
 import os
 from datetime import datetime
-import unittest
 from models.base_model import BaseModel
-import models.base_model
-from models.engine.file_storage import FileStorage
-import models
 
 
-class TestStorageDocumentation(unittest.TestCase):
-    """ Create a tests for the file storage in documentation
-    and requirements """
+class test_storage(unittest.TestCase):
+    """Requirements cases
+    """
+    def test_to_the_module_docstring(self):
+        self.assertTrue(len(file_storage.FileStorage.__doc__) > 1)
 
-    def test_style_pep8_model(self):
-        """ PEP8 python style """
-        style_model = os.system("pep8 models/engine/file_storage.py")
-        self.assertEqual(style_model, 0)
+    def test_to_the_class_docstring(self):
+        self.assertTrue(len(file_storage.FileStorage.__doc__) > 1)
 
-    def test_style_pep8(self):
-        """ PEP8 python style """
-        style_test = os.system("pep8 tests/test_models/test_engine\
-/test_file_storage.py")
-        self.assertEqual(style_test, 0)
+    def test_to_the_class_docstring(self):
+        self.assertTrue(len(file_storage.FileStorage.__doc__) > 1)
+
+    def test_of_PEP8_base_model(self):
+        self.assertEqual(os.system("pep8 ./models/engine/file_storage.py"), 0)
+
+    def test_of_PEP8_test_base_model(self):
+        self.assertEqual(os.system(
+            "pep8 tests/test_models/test_engine/test_file_storage.py"), 0)
 
     def test_shebang(self):
-        """ Test shebang in the front line """
-        with open("models/engine/file_storage.py", mode='r') as _file:
-            readShebang = _file.read()
-            lines = readShebang.splitlines()
-            self.assertEqual(lines[0], '#!/usr/bin/python3')
+        with open('models/engine/file_storage.py', 'r') as text:
+            line_1 = text.readline()
+            self.assertEqual(line_1.strip(), '#!/usr/bin/python3')
 
-    def test_shebang_test(self):
-        """ Test shebang in the front line in test file """
-        with open("tests/test_models/test_engine/test_file_storage.py\
-", mode='r') as _file:
-            readShebang = _file.read()
-            lines = readShebang.splitlines()
-            self.assertEqual(lines[0], '#!/usr/bin/python3')
-
-    def test_module_doc(self):
-        """ Module with sufficient documentation """
-        self.assertTrue(len(models.engine.file_storage.__doc__) != 0)
-
-    def test_class_doc(self):
-        """ Class with sufficient documentation """
-        self.assertTrue(len(FileStorage.__doc__) != 0)
-
-    def test_methods_doc(self):
-        """ Methods with sufficient documentation """
-        self.assertTrue(len(FileStorage.all.__doc__) != 0)
-        self.assertTrue(len(FileStorage.new.__doc__) != 0)
-        self.assertTrue(len(FileStorage.save.__doc__) != 0)
-        self.assertTrue(len(FileStorage.reload.__doc__) != 0)
-
-
-class TestStorage(unittest.TestCase):
-    """ Create a tests for the file storage in edge cases """
-    def test_attribute_path(self):
-        """ Check to the path to the JSON file that is a private
-            class attributes """
-        self.assertEqual(FileStorage._FileStorage__file_path, "file.json")
-        self.assertTrue(FileStorage._FileStorage__file_path != 0)
-        self.assertTrue(type(FileStorage._FileStorage__file_path) is str)
-
-    def test_attribute_class_objects(self):
-        """Check to the dictionary will store all objects that is a private"""
-        self.assertTrue(type(FileStorage._FileStorage__objects) is dict)
-
+    """Class cases
+    """
     def test_all(self):
-        """ Check that method returns the dictionary __objects """
-        object = models.storage.all()
-        self.assertIsInstance(object, dict)
+        my_objs = storage.all()
+        self.assertTrue(type(my_objs), "<class 'dict'>")
 
-    def test_new(self):
-        """ Check that method sets in __objects the obj with key
-            <obj class name>.id """
-        pass
-
-    def test_save_exists(self):
-        """ Serialize __objects to the JSON file """
-        file = "obj.json"
-        self.assertFalse(os.path.exists(file))
-        FileStorage._FileStorage__file_path = "obj.json"
-        obj = BaseModel()
-        obj.save()
-        self.assertTrue(os.path.exists(file))
-        os.remove(file)
+    def test_save(self):
+        my_file = "file.json"
+        os.remove(my_file)
+        MyModel = BaseModel()
+        storage.save()
+        self.assertTrue(os.path.exists(my_file), True)
+        os.remove(my_file)
 
     def test_reload(self):
-        """ Deserialize the JSON file to __objects """
-        pass
+        MyModel = BaseModel()
+        storage.save()
+        _dic = storage.reload
+        self.assertTrue(type(_dic), "<class 'dict'>")
