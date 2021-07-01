9c3cbce04f3d79f594f3be8a3e8c80f67205c340
Brian Zapata <dzapata397@gmail.com>
2021-07-01 04:57:50 +0000

    Add the test to the attrs of the storage class Commit

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,6 +9,8 @@ import os
 from datetime import datetime
 from models.base_model import BaseModel
 
+FileStorage = file_storage.FileStorage
+
 
 class test_storage(unittest.TestCase):
     """Requirements cases
@@ -17,10 +19,7 @@ class test_storage(unittest.TestCase):
         self.assertTrue(len(file_storage.FileStorage.__doc__) > 1)
 
     def test_to_the_class_docstring(self):
-        self.assertTrue(len(file_storage.FileStorage.__doc__) > 1)
-
-    def test_to_the_class_docstring(self):
-        self.assertTrue(len(file_storage.FileStorage.__doc__) > 1)
+        self.assertTrue(len(FileStorage.__doc__) > 1)
 
     def test_of_PEP8_base_model(self):
         self.assertEqual(os.system("pep8 ./models/engine/file_storage.py"), 0)
@@ -53,3 +52,10 @@ class test_storage(unittest.TestCase):
         storage.save()
         _dic = storage.reload
         self.assertTrue(type(_dic), "<class 'dict'>")
+
+    def test_attr_path(self):
+        self.assertTrue(type(FileStorage._FileStorage__file_path), ".json")
+
+    def test_attr_objects(self):
+        self.assertTrue(type(
+            FileStorage._FileStorage__objects), dict)
